66c326c42abd70cf3ecee859ca99edbb51a0093e
Patryk <patryk.r.nowak@gmail.com>
2021-09-03 21:27:27 +0100

    next project

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: {
+    enabled: true, // false
+    content: ['./dist/**/*.html'], //'./dist/**/*.php'
+    },
+  darkMode: 'class', // or 'media - is going to allow device to control the dark mode' or 'class'false
+  theme: {
+    debugScreens: {
+      position: ['top', 'left']
+    },
+    extend: {
+      fontFamily: {
+        // by this we defined our own font -> nice
+        headline: ["Oswald"]
+      },
+      colors: {
+        // mainColor is our name, we can put it whatever we want
+        // so we extended again the tailwin! nice!
+        mainColor: '#1E293B'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-debug-screens')
+  ],
+}
