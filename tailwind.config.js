099defe5cf32e9170043d364a0494a8a58e347ec
Patryk <patryk.r.nowak@gmail.com>
2021-09-04 10:50:49 +0100

    exporting and importing styles from and to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,26 @@
+// importing additional color palette
+// const colors = require("tailwindcss/colors");
+
 module.exports = {
+  presets: [
+    require('./company_styles')
+  ],
   purge: {
-    enabled: true, // false
-    content: ['./dist/**/*.html'], //'./dist/**/*.php'
-    },
-  darkMode: 'class', // or 'media - is going to allow device to control the dark mode' or 'class'false
+    enabled: false, // false
+    content: ["./dist/**/*.html"], //'./dist/**/*.php'
+  },
+  darkMode: "class", // or 'media - is going to allow device to control the dark mode' or 'class'false
   theme: {
     debugScreens: {
-      position: ['top', 'left']
+      position: ["top", "left"],
     },
-    // spacing: {
-    //     13: '3.25rem' //total overwrite
-    //   },
+
     extend: {
-      fontFamily: {
-        // by this we defined our own font -> nice
-        headline: ["Oswald"]
-      },
-      colors: {
-        // mainColor is our name, we can put it whatever we want
-        // so we extended again the tailwin! nice!
-        mainColor: '#1E293B'
-      },
-      // spacing: {
-      //   13: '3.25rem' 
-      // },
+
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('tailwindcss-debug-screens')
-  ],
-}
+  plugins: [require("tailwindcss-debug-screens")],
+};
