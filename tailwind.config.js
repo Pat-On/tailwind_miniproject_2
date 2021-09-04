340a0e80d02a32fca6482b0ccb68eb555eb65e3e
Patryk <patryk.r.nowak@gmail.com>
2021-09-04 12:13:45 +0100

    breakpoints plus container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,40 @@
 // const colors = require("tailwindcss/colors");
 
 module.exports = {
-  presets: [
-    require('./company_styles')
-  ],
+  presets: [require("./news_styles")],
   purge: {
     enabled: false, // false
     content: ["./dist/**/*.html"], //'./dist/**/*.php'
+  },
+  // it works
+  corePlugins: {
+    // float: false,
+    container: false, // we have no longer predefined class container
   },
   darkMode: "class", // or 'media - is going to allow device to control the dark mode' or 'class'false
   theme: {
     debugScreens: {
       position: ["top", "left"],
     },
-
-    extend: {
-
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "0.5rem",
+        sm: "1rem",
+        lg: "1.5rem",
+        xl: "2rem",
+        "2xl": "4rem",
+      },
     },
-  },
-  variants: {
     extend: {},
+  },
+  // extending variants! nice!
+  variants: {
+    // if we are going to put something not inside extend we will have posibility to remove variants
+    backgroundColor: ['dark', 'group-hover', 'focus-within', 'hover', 'focus'],
+    extend: {
+      width: ['hover']
+    },
   },
   plugins: [require("tailwindcss-debug-screens")],
 };
